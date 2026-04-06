0cdbf045e107383a88f850ec0ceac26820206e14
Yokasashii <vincentguerrini07@gmail.com>
2026-04-06 18:05:29 +0200

    add: diffrentBackground

--- JS/stat.js
+++ JS/stat.js
@@ -1,8 +1,44 @@
 const canvas1 = document.getElementById("canvas1")
 const c1 = canvas1.getContext('2d')
 
-const BackgroundImg = new Image()
-BackgroundImg.src = "../Assets/Background/Sprite - StateSheet.png"
+const Background1Img = new Image()
+const Background2Img = new Image()
+const Background3Img = new Image()
+const Background4Img = new Image()
+const Background5Img = new Image()
+const Background6Img = new Image()
+const Background7Img = new Image()
+const Background8Img = new Image()
+const Background9Img = new Image()
+const Background10Img = new Image()
+
+Background1Img.src = "../Assets/Background/Stats/Sprite - StateSheet1.png"
+Background2Img.src = "../Assets/Background/Stats/Sprite - StateSheet2.png"
+Background3Img.src = "../Assets/Background/Stats/Sprite - StateSheet3.png"
+Background4Img.src = "../Assets/Background/Stats/Sprite - StateSheet4.png"
+Background5Img.src = "../Assets/Background/Stats/Sprite - StateSheet5.png"
+Background6Img.src = "../Assets/Background/Stats/Sprite - StateSheet6.png"
+Background7Img.src = "../Assets/Background/Stats/Sprite - StateSheet7.png"
+Background8Img.src = "../Assets/Background/Stats/Sprite - StateSheet8.png"
+Background9Img.src = "../Assets/Background/Stats/Sprite - StateSheet9.png"
+Background10Img.src = "../Assets/Background/Stats/Sprite - StateSheetX.png"
+
+const backgroundByLevel = {
+    1: Background1Img,
+    2: Background2Img,
+    3: Background3Img,
+    4: Background4Img,
+    5: Background5Img,
+    6: Background6Img,
+    7: Background7Img,
+    8: Background8Img,
+    9: Background9Img,
+    10: Background10Img
+}
+
+function getBackgroundForLevel(level) {
+    return backgroundByLevel[level] || Background10Img
+}
 
 //stat era configuration
 
@@ -14,14 +50,16 @@ BackgroundImg.src = "../Assets/Background/Sprite - StateSheet.png"
 function drawStats(){
 
     statSize()
+    const currentLevel = player ? player.getLevel() : 1
+    const actualBackground = getBackgroundForLevel(currentLevel)
 
-    if (BackgroundImg.complete && BackgroundImg.naturalWidth > 0) {
-        const scale = Math.max(canvas1.width / BackgroundImg.width, canvas1.height / BackgroundImg.height)
-        const drawWidth = BackgroundImg.width * scale
-        const drawHeight = BackgroundImg.height * scale
+    if (actualBackground.complete && actualBackground.naturalWidth > 0) {
+        const scale = Math.max(canvas1.width / actualBackground.width, canvas1.height / actualBackground.height)
+        const drawWidth = actualBackground.width * scale
+        const drawHeight = actualBackground.height * scale
         const offsetX = (canvas1.width - drawWidth) / 2
         const offsetY = (canvas1.height - drawHeight) / 2
-        c1.drawImage(BackgroundImg, offsetX, offsetY, drawWidth, drawHeight)
+        c1.drawImage(actualBackground, offsetX, offsetY, drawWidth, drawHeight)
     }
  
     c1.fillStyle = "white"
