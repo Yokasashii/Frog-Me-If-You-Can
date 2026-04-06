ce09b44efbcf9abb55ffe1d34ab908faf7051d4c
Yokasashii <vincentguerrini07@gmail.com>
2026-04-07 00:12:43 +0200

    add: InputBackground

--- JS/inGame/action.js
+++ JS/inGame/action.js
@@ -1,6 +1,9 @@
 const canvas2 = document.getElementById("canvas2")
 const c2 = canvas2.getContext('2d')
 
+const BackgroundInput = new Image()
+BackgroundInput.src = "../Assets/Background/Menu/Sprite - InputSheet.png"
+
 //configurattion 
 universalCanvasSize(canvas2,9.91/10,1.7/10)
 
@@ -11,53 +14,24 @@ c2.font = "30px Arial";
 
 c2.fillStyle = "white";
 
-c2.fillText("Touche :", 30, 50);
+function drawBackground() {
+    c2.fillRect(0,0, canvas2.width, canvas2.height)
 
-// all type of button
-const action1 = {
-    x: 25,
-    y: 60,
-    width: 250,
-    height:100,
-    message: "espace attaque"
+    const scale = Math.min(
+        canvas2.width / BackgroundInput.naturalWidth,
+        canvas2.height / BackgroundInput.naturalHeight
+    );
+
+    const drawWidth = BackgroundInput.naturalWidth * scale;
+    const drawHeight = BackgroundInput.naturalHeight * scale;
+    const offsetX = (canvas2.width - drawWidth) / 2;
+    const offsetY = (canvas2.height - drawHeight) / 2;
+
+    c2.drawImage(BackgroundInput, offsetX, offsetY, drawWidth, drawHeight);
 }
 
-const action2 = {
-    x: 285,
-    y: 60,
-    width: 250,
-    height:100,
-    message: "1 Chocolat"
+BackgroundInput.onload = drawBackground
+
+if (BackgroundInput.complete && BackgroundInput.naturalWidth > 0) {
+    drawBackground()
 }
-
-const action3 = {
-    x: 545,
-    y: 60,
-    width: 250,
-    height:100,
-    message: "2 café"
-}
-
-const action4 = {
-    x: 805,
-    y: 60,
-    width: 250,
-    height:100,
-    message: "3 Amélioraton"
-}
-
-const action5 = {
-    x: 1065,
-    y: 60,
-    width: 250,
-    height:100,
-    message: "4 Abandonner"
-}
-
-// draw all button
-
-universalDrawButton(c2, action1)
-universalDrawButton(c2, action2)
-universalDrawButton(c2, action3)
-universalDrawButton(c2, action4)
-universalDrawButton(c2, action5)
